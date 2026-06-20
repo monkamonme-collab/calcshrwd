7b5272f6fc0f108f6588d9ac65fe4840b92cf37c
monkamonme-collab <monkamonme@gmail.com>
2026-06-21 00:54:35 +0800

    Fix: use www subdomain for sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://calcshrwd.com/sitemap.xml",
+    sitemap: "https://www.calcshrwd.com/sitemap.xml",
   };
 }
